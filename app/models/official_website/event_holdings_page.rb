b29c5fd84fac897bbe3e79cbffd14ac1b6ee480a
Johndoe <56298669+togane@users.noreply.github.com>
2021-04-17 16:36:01 +0900

    bring page models from old repository

---
+++ app/models/official_website/event_holdings_page.rb
@@ -0,0 +1,11 @@
+module OfficialWebsite
+  class EventHoldingsPage < Page
+    attribute :date, :date
+
+    def params
+      {
+        date: date,
+      }
+    end
+  end
+end
---
+++ app/models/official_website/racer_profile_page.rb
@@ -0,0 +1,11 @@
+module OfficialWebsite
+  class RacerProfilePage < Page
+    attribute :racer_registration_number, :integer
+
+    def params
+      {
+        registration_number: racer_registration_number,
+      }
+    end
+  end
+end
