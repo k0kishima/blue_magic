4bcbe95ba97f33bb379b18f16c9ea4abb2ed8c1e
Johndoe <56298669+togane@users.noreply.github.com>
2021-04-21 21:26:54 +0900

    implement csv factory

---
+++ app/models/csv_factory.rb
@@ -0,0 +1,15 @@
+require 'csv'
+
+class CsvFactory
+  def self.create(array_of_hash)
+    Tempfile.new([SecureRandom.uuid, '.csv']).tap do |file|
+      header = array_of_hash.first.keys
+      rows = array_of_hash.map{|hash| hash.values }
+
+      CSV.open(file, 'wb') do |csv|
+        csv << header
+        rows.each { |row| csv << row }
+      end
+    end
+  end
+end
