e56b3cc66053f6ebe770259486b166cc11a93a4a
Johndoe <56298669+togane@users.noreply.github.com>
2021-04-23 08:12:17 +0900

    add events importer

--- app/importers/base_importer.rb
+++ app/importers/base_importer.rb
@@ -1,20 +1,12 @@
 class BaseImporter
-  include ScrapingSubscribable
-
-  def import!(csv)
-    parsed_attributes = parser_class.new(csv).parse!
-    resources = parsed_attributes.map{|attributes| model_class.new(attributes) }
-    model_class.import!(resources, all_or_none: true)
+  def import!(parsed_attributes)
+    resources = parsed_attributes.map { |attributes| model_class.new(attributes) }
+    model_class.import!(resources, all_or_none: true, on_duplicate_key_update: on_duplicate_key_update)
   end
 
   private
-
-  def parser_class
-    raise NotImplementedError
-  end
 
   def model_class
     raise NotImplementedError
   end
 end
-
