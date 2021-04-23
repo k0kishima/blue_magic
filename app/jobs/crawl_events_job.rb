12ecadeee5d0d6fd29352189e2cacd37b88598fd
Johndoe <56298669+togane@users.noreply.github.com>
2021-04-24 04:53:07 +0900

    fix crawl job namespace

--- app/jobs/crawl_events_job.rb
+++
@@ -1,7 +0,0 @@
-class CrawlEventsJob < CrawlJob
-  def perform(year:, month:, version: DEFAULT_VERSION)
-    page = OfficialWebsite::EventSchedulePage.new(version: version, year: year, month: month, no_cache: no_cache)
-    crawler = Crawler.new(page)
-    crawler.crawl!
-  end
-end
