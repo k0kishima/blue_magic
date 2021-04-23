e86d2532efcb4d89a21f6bff18d2397ae94d438f
Johndoe <56298669+togane@users.noreply.github.com>
2021-04-23 20:29:26 +0900

    use sidekiq as queue adapter

--- app/models/official_website/page.rb
+++ app/models/official_website/page.rb
@@ -9,13 +9,13 @@ require 'open-uri'
 module OfficialWebsite
   class Page
     BASE_URL = Rails.application.config.x.official_website_proxy.base_url
-    USE_VERSION = Rails.application.config.x.official_website_proxy.latest_official_website_version
+    DEFAULT_VERSION = Rails.application.config.x.official_website_proxy.latest_official_website_version
 
     include ActiveModel::Model
     include ActiveModel::Attributes
     include ActiveModel::Validations
 
-    attribute :version, :integer, default: USE_VERSION
+    attribute :version, :integer, default: DEFAULT_VERSION
     attribute :no_cache, :boolean, default: false
 
     def file
