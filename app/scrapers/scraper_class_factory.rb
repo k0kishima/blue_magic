a19926caa721cdf74335d4ecf85b7bb8a3ea8a00
Johndoe <56298669+togane@users.noreply.github.com>
2021-04-24 05:48:48 +0900

    fix scraper class factory

--- app/scrapers/scraper_class_factory.rb
+++ app/scrapers/scraper_class_factory.rb
@@ -4,6 +4,22 @@ class ScraperClassFactory
     case page.class.name
     when 'OfficialWebsite::EventSchedulePage'
       [OfficialWebsite::V1707::EventsScraper]
+    when 'OfficialWebsite::RacerProfilePage'
+      [OfficialWebsite::V1707::RacerProfilesScraper]
+    when 'OfficialWebsite::EventHoldingsPage'
+      [OfficialWebsite::V1707::EventHoldingsScraper]
+    when 'OfficialWebsite::EventEntriesPage'
+      [OfficialWebsite::V1707::EventEntriesScraper]
+    when 'OfficialWebsite::RaceInformationPage'
+      [OfficialWebsite::V1707::RaceInformationsScraper, OfficialWebsite::V1707::RaceEntriesScraper]
+    when 'OfficialWebsite::RaceExhibitionInformationPage'
+      [OfficialWebsite::V1707::RaceExhibitionRecordsScraper, OfficialWebsite::V1707::RacerConditionsScraper,
+       OfficialWebsite::V1707::BoatSettingsScraper, OfficialWebsite::V1707::MotorMaintenancesScraper,
+       OfficialWebsite::V1707::WeatherConditionsScraper]
+    when 'OfficialWebsite::RaceResultPage'
+      [OfficialWebsite::V1707::RaceRecordsScraper, OfficialWebsite::V1707::PayoffsScraper]
+    when 'OfficialWebsite::RaceOddsPage'
+      [OfficialWebsite::V1707::OddsScraper]
     else
       raise NotImplementedError
     end
