fa67eef792d5026814d02f45d0fbcd651a32666b
Johndoe <56298669+togane@users.noreply.github.com>
2021-04-24 14:18:50 +0900

    add boat_betting_contribute_rate_aggregation_parser

--- spec/parsers/race_entry_list_parser_spec.rb
+++ spec/parsers/race_entry_list_parser_spec.rb
@@ -12,14 +12,20 @@ describe RaceEntryListParser do
 
       it 'returns array of dict as result of csv parsing' do
         expect(subject).to contain_exactly(
-          {:date=>Date.new(2018, 3, 1), :stadium_tel_code=>7, :race_number=>8, :racer_registration_number=>4190, :pit_number=>1},
-          {:date=>Date.new(2018, 3, 1), :stadium_tel_code=>7, :race_number=>8, :racer_registration_number=>4240, :pit_number=>2},
-          {:date=>Date.new(2018, 3, 1), :stadium_tel_code=>7, :race_number=>8, :racer_registration_number=>4419, :pit_number=>3},
-          {:date=>Date.new(2018, 3, 1), :stadium_tel_code=>7, :race_number=>8, :racer_registration_number=>3175, :pit_number=>4},
-          {:date=>Date.new(2018, 3, 1), :stadium_tel_code=>7, :race_number=>8, :racer_registration_number=>3254, :pit_number=>5},
-          {:date=>Date.new(2018, 3, 1), :stadium_tel_code=>7, :race_number=>8, :racer_registration_number=>4843, :pit_number=>6})
+          { :date => Date.new(2018, 3, 1), :stadium_tel_code => 7, :race_number => 8,
+            :racer_registration_number => 4190, :pit_number => 1 },
+          { :date => Date.new(2018, 3, 1), :stadium_tel_code => 7, :race_number => 8,
+            :racer_registration_number => 4240, :pit_number => 2 },
+          { :date => Date.new(2018, 3, 1), :stadium_tel_code => 7, :race_number => 8,
+            :racer_registration_number => 4419, :pit_number => 3 },
+          { :date => Date.new(2018, 3, 1), :stadium_tel_code => 7, :race_number => 8,
+            :racer_registration_number => 3175, :pit_number => 4 },
+          { :date => Date.new(2018, 3, 1), :stadium_tel_code => 7, :race_number => 8,
+            :racer_registration_number => 3254, :pit_number => 5 },
+          { :date => Date.new(2018, 3, 1), :stadium_tel_code => 7, :race_number => 8,
+            :racer_registration_number => 4843, :pit_number => 6 }
+        )
       end
     end
   end
 end
-
