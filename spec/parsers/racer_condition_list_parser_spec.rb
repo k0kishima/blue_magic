e7dc2c20c8111d73bb2edfa5a887c3cade94ea0d
Johndoe <56298669+togane@users.noreply.github.com>
2021-04-25 09:48:48 +0900

    fix weather condition list parser

--- spec/parsers/racer_condition_list_parser_spec.rb
+++ spec/parsers/racer_condition_list_parser_spec.rb
@@ -12,12 +12,12 @@ describe RacerConditionListParser do
 
       it 'returns array of dict as result of csv parsing' do
         expect(subject).to contain_exactly(
-          {:date=>Date.new(2015, 11, 16), :racer_registration_number=>4096, :weight=>52.5, :adjust=>0.0},
-          {:date=>Date.new(2015, 11, 16), :racer_registration_number=>4693, :weight=>51.0, :adjust=>0.0},
-          {:date=>Date.new(2015, 11, 16), :racer_registration_number=>2505, :weight=>50.0, :adjust=>1.0},
-          {:date=>Date.new(2015, 11, 16), :racer_registration_number=>4803, :weight=>54.4, :adjust=>0.0},
-          {:date=>Date.new(2015, 11, 16), :racer_registration_number=>3138, :weight=>51.9, :adjust=>0.0},
-          {:date=>Date.new(2015, 11, 16), :racer_registration_number=>4221, :weight=>50.0, :adjust=>1.0}
+          { :date => Date.new(2015, 11, 16), :racer_registration_number => 4096, :weight => 52.5, :adjust => 0.0 },
+          { :date => Date.new(2015, 11, 16), :racer_registration_number => 4693, :weight => 51.0, :adjust => 0.0 },
+          { :date => Date.new(2015, 11, 16), :racer_registration_number => 2505, :weight => 50.0, :adjust => 1.0 },
+          { :date => Date.new(2015, 11, 16), :racer_registration_number => 4803, :weight => 54.4, :adjust => 0.0 },
+          { :date => Date.new(2015, 11, 16), :racer_registration_number => 3138, :weight => 51.9, :adjust => 0.0 },
+          { :date => Date.new(2015, 11, 16), :racer_registration_number => 4221, :weight => 50.0, :adjust => 1.0 }
         )
       end
     end
