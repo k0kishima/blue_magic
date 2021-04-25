d409e9e430714edd2c45b52525a6ed67871a401d
Johndoe <56298669+togane@users.noreply.github.com>
2021-04-26 05:34:08 +0900

    add odds parser

---
+++ app/parsers/odds_list_parser.rb
@@ -0,0 +1,23 @@
+class OddsListParser < BaseParser
+  HEADER_KEYS = [
+    :date,
+    :stadium_tel_code,
+    :race_number,
+    :betting_number,
+    :ratio,
+  ]
+
+  def parse!
+    validate_header_keys!
+
+    rows.map do |row|
+      {
+        date: row[0].to_date,
+        stadium_tel_code: row[1].to_i,
+        race_number: row[2].to_i,
+        betting_number: row[3].to_i,
+        ratio: row[4].to_f,
+      }
+    end
+  end
+end
