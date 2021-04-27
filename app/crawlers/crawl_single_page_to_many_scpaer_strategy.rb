7719cef54d5018fa025062b51c63b6369f5b9718
Johndoe <56298669+togane@users.noreply.github.com>
2021-04-27 20:15:27 +0900

    divide crawl processing into strategy classes

---
+++ app/crawlers/crawl_single_page_to_many_scpaer_strategy.rb
@@ -0,0 +1,33 @@
+class CrawlSinglePageToManyScpaerStrategy
+  def initialize(source_page)
+    @source_page = source_page
+  end
+
+  def crawl!
+    available_scraper_classes = ScraperClassFactory.bulk_create!(source_page, context: :single_page)
+    available_scraper_classes.each do |scraper_class|
+      scraper = scraper_class.new(file: source_page.file)
+
+      begin
+        csv = CsvFactory.create!(scraper.scrape!)
+
+        available_parser_classes = ParserClassFactory.bulk_create!(scraper)
+        available_parser_classes.each do |parser_class|
+          parser = parser_class.new(csv)
+          available_importer_class = ImporterClassFactory.create!(parser)
+          available_importer_class.new.import!(parser.parse!)
+        end
+      ensure
+        csv.close
+      end
+    end
+
+    true
+  ensure
+    source_page.file.close
+  end
+
+  private
+
+  attr_reader :source_page
+end
