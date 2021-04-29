c1eb7896977e6ab2581ff8e2bb7be08ac56757fa
Johndoe <56298669+togane@users.noreply.github.com>
2021-04-30 05:12:47 +0900

    fix parser class factory

--- app/parsers/parser_class_factory.rb
+++ app/parsers/parser_class_factory.rb
@@ -3,78 +3,17 @@ require 'csv'
 # TODO: バージョン追加された時点で修正する
 class ParserClassFactory
   class << self
-    def bulk_create!(object)
-      strategy = case
-                 when object.is_a?(Scraper)
-                   CreateFromScraperStrategy.new(object)
-                 when object.is_a?(Tempfile)
-                   CreateFromCsvStrategy.new(object)
-                 else
-                   raise NotImplementedError.new("#{context} is invalid context to create parsers")
-                 end
-      strategy.bulk_create!
+    def bulk_create(csv)
+      csv = CSV.read(csv)
+
+      # HACK: これやらないと未使用のクラスが読み込まれる機会がないためObjectSpaceにサブクラスが入らなくて .subclasses や .descendants の返り値が空になってしまう
+      # Rails.application.eager_load! で関係ないファイル全部読むよりはマシではあるのでワークアラウンドとして使用
+      Dir[Rails.root.join('app', 'parsers', '*.rb')].each { |f| require f }
+  
+      header = csv.first.map(&:to_sym)
+      BaseParser.descendants.select do |parser_class|
+        parser_class::HEADER_KEYS == header
+      end
     end
   end
-end
-
-class CreateFromScraperStrategy
-  def initialize(scraper)
-    @scraper = scraper
-  end
-
-  def bulk_create!
-    case scraper.class.name
-    when 'OfficialWebsite::V1707::EventsScraper'
-      [EventListParser]
-    when 'OfficialWebsite::V1707::RacerProfilesScraper'
-      [RacerListParser]
-    when 'OfficialWebsite::V1707::RaceInformationsScraper'
-      [RaceListParser]
-    when 'OfficialWebsite::V1707::RaceEntriesScraper'
-      [RaceEntryListParser, BoatBettingContributeRateAggregationParser, MotorBettingContributeRateAggregationParser,
-       RacerWinningRateAggregationParser]
-    when 'OfficialWebsite::V1707::RaceExhibitionRecordsScraper'
-      [RaceExhibitionRecordsParser]
-    when 'OfficialWebsite::V1707::RacerConditionsScraper'
-      [RacerConditionListParser]
-    when 'OfficialWebsite::V1707::WeatherConditionsScraper'
-      [WeatherConditionListParser]
-    when 'OfficialWebsite::V1707::RaceRecordsScraper'
-      [RaceRecordListParser, DisqualifiedRaceEntryListParser, WinningRaceEntryListParser]
-    when 'OfficialWebsite::V1707::PayoffsScraper'
-      [PayoffListParser]
-    when 'OfficialWebsite::V1707::OddsScraper'
-      [OddsListParser]
-    else
-      raise NotImplementedError
-    end
-  end
-
-  private
-
-  attr_reader :scraper
-end
-
-class CreateFromCsvStrategy
-  def initialize(csv)
-    @csv = CSV.read(csv)
-  end
-
-  def bulk_create!
-    # HACK: BaseImporter.subclesses が意図しない動作をする（空の配列返してくる）ので以下のように定数を直接指定して突合
-    header = csv.first.map(&:to_sym)
-
-    case header
-    when BoatSettingListParser::HEADER_KEYS.map(&:to_sym)
-      [BoatSettingListParser]
-    when MotorMaintenanceListParser::HEADER_KEYS.map(&:to_sym)
-      [MotorMaintenanceListParser]
-    else
-      raise NotImplementedError
-    end
-  end
-
-  private
-
-  attr_reader :csv
 end
