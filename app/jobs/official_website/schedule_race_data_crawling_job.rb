19d944f277163e8fe71c444f3dea47c34f562149
Johndoe <56298669+togane@users.noreply.github.com>
2021-04-30 20:22:07 +0900

    implement a job which schedules race data crawling

---
+++ app/jobs/official_website/schedule_race_data_crawling_job.rb
@@ -0,0 +1,32 @@
+module OfficialWebsite
+  class ScheduleRaceDataCrawlingJob < CrawlJob
+    # NOTE: レースの基本情報も再取得されるが、投票締め切り時刻が変わったり中止になったりすることはあるので問題ない
+    CRAWL_RACE_DATA_JOBS = [
+      CrawlRaceInformationsJob,
+      CrawlRaceExhibitionInformationsJob,
+      CrawlRaceResultsJob,
+      CrawlOddsJob,
+      CrawlBoatSettingsJob,
+    ]
+
+    def perform(date: Date.today, version: DEFAULT_VERSION)
+      # NOTE: 締め切り時刻を基準にクロールするので、そのデータが先に存在している必要がある
+      # この時間的序列が生じるのは止むを得ないか・・・
+      races = Race.where(date: date)
+      raise ArgumentError.new('races not found in specified date') if races.blank?
+
+      races.each do |race|
+        CRAWL_RACE_DATA_JOBS.each do |crawl_data_job|
+          crawl_data_job
+            .set(wait_until: race.betting_deadline_at - 10.minutes)
+            .perform_later(
+              stadium_tel_code: race.stadium_tel_code,
+              race_opened_on: race.date,
+              race_number: race.race_number,
+              version: version
+            )
+        end
+      end
+    end
+  end
+end
---
+++ spec/jobs/official_website/schedule_race_data_crawling_job_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+describe OfficialWebsite::ScheduleRaceDataCrawlingJob, type: :job do
+  describe '#perform_now' do
+    subject { described_class.perform_now(date: date) }
+
+    let(:date) { Date.new(2021, 4, 6) }
+
+    context 'when races exist in specified date' do
+      let!(:race_1) { create(:race, date: date, stadium_tel_code: 3, race_number: 1) }
+      let!(:race_2) { create(:race, date: date, stadium_tel_code: 4, race_number: 1) }
+      let(:race_count) { Race.where(date: date).count }
+
+      before do
+        ActiveJob::Base.queue_adapter = :test
+      end
+
+      it 'invokes CrawlRaceInformationsJob exactly races count' do
+        expect { subject }.to have_enqueued_job(OfficialWebsite::CrawlRaceInformationsJob).exactly(race_count).times
+      end
+
+      it 'invokes CrawlRaceExhibitionInformationsJob exactly races count' do
+        expect {
+          subject
+        }.to have_enqueued_job(OfficialWebsite::CrawlRaceExhibitionInformationsJob).exactly(race_count).times
+      end
+
+      it 'invokes CrawlRaceResultsJob exactly races count' do
+        expect { subject }.to have_enqueued_job(OfficialWebsite::CrawlRaceResultsJob).exactly(race_count).times
+      end
+
+      it 'invokes CrawlOddsJob exactly races count' do
+        expect { subject }.to have_enqueued_job(OfficialWebsite::CrawlOddsJob).exactly(race_count).times
+      end
+
+      it 'invokes CrawlBoatSettingsJob exactly races count' do
+        expect { subject }.to have_enqueued_job(OfficialWebsite::CrawlBoatSettingsJob).exactly(race_count).times
+      end
+    end
+
+    context 'when races do not exist in specified date' do
+      it { expect { subject }.to raise_error(ArgumentError) }
+    end
+  end
+end
