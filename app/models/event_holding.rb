ac7138ca34f6040d8fe5d6275f4bd40922d426aa
Johndoe <56298669+togane@users.noreply.github.com>
2021-04-30 12:39:18 +0900

    add a scope to event holding model

--- app/models/event_holding.rb
+++ app/models/event_holding.rb
@@ -8,4 +8,20 @@ class EventHolding
 
   validates :stadium_tel_code, presence: true, inclusion: { in: Stadium::TELCODE_RANGE }
   validates :date, presence: true
+
+  def self.opened_on(date)
+    page = OfficialWebsite::EventHoldingsPage.new(date: date)
+
+    begin
+      scraper_class = OfficialWebsite::ScraperClassFactory.create!(self.name)
+      scraper = scraper_class.new(file: page.file)
+      scraper.scrape!.reject do |hash|
+        hash.fetch(:day_text).include?('中止')
+      end.map do |hash|
+        new(date: date, stadium_tel_code: hash.fetch(:stadium_tel_code))
+      end
+    ensure
+      page.file.close
+    end
+  end
 end
