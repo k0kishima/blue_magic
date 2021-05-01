76f947b5f0b98dab08517a6f4148ba06d325eb4f
Johndoe <56298669+togane@users.noreply.github.com>
2021-05-01 10:45:45 +0900

    fix how to handle data not found exception in weather condition scraper

--- app/scrapers/concerns/official_website/v1707/no_contents_handleable.rb
+++ app/scrapers/concerns/official_website/v1707/no_contents_handleable.rb
@@ -3,15 +3,13 @@ module OfficialWebsite
     module NoContentsHandleable
       extend ActiveSupport::Concern
 
-      DATA_NOT_FOUND_TEXT = 'データがありません。'
-
       included do
         def raise_exception_if_data_not_found!
           raise ::DataNotFound.new if data_not_found?
         end
 
         def data_not_found?
-          html.search('.l-main').text.match(/#{DATA_NOT_FOUND_TEXT}/).present?
+          html.search('.l-main')&.text&.match(/データ[がは]ありません/).present?
         end
       end
     end
