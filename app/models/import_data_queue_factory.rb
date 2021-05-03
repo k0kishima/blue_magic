51bbea75decdde5d0a2eff31dee61e3c662df42e
Johndoe <56298669+togane@users.noreply.github.com>
2021-05-03 12:50:09 +0900

    fix how to handle errors when create import data queue

--- app/models/import_data_queue_factory.rb
+++ app/models/import_data_queue_factory.rb
@@ -2,18 +2,28 @@ class ImportDataQueueFactory
   def self.create!(scraper_class, *pages)
     raise ArgumentError.new('there is difference between page types') unless pages.map(&:class).same?
 
+    slack_client = Slack::Web::Client.new
+
     data = []
     pages.each do |page|
       begin
         data << scraper_class.new(file: page.file).scrape!
       rescue DataNotFound
-        Rails.logger.debug("this race has been skipped because data not found (url: #{page.origin_redirection_url})")
+        unless page.no_cache
+          page.no_cache = true
+          retry
+        else
+          slack_client
+            .chat_postMessage(channel: '001_crawler_emergency',
+                              text: "need to confirm that it's error or not. (url: #{page.origin_redirection_url})")
+        end
       rescue RaceCanceled
-        Rails.logger.debug("this race has been canceled because data not found (url: #{page.origin_redirection_url})")
+        CancelRaceJob.perform_later(stadium_tel_code: page.stadium_tel_code, date: page.race_opened_on,
+                                    race_number: page.race_number)
       rescue StandardError => e
-        slack_client ||= Slack::Web::Client.new
-        slack_client.chat_postMessage(channel: '001_crawler_emergency',
-                                      text: "#{e.message} (url: #{page.origin_redirection_url})")
+        slack_client
+          .chat_postMessage(channel: '001_crawler_emergency',
+                            text: "#{e.message} (url: #{page.origin_redirection_url})")
       ensure
         page.file.close
       end
