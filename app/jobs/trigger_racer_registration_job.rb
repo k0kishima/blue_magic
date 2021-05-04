4ab6ba593541c0e57ef2c01aee530e2f35970e84
Johndoe <56298669+togane@users.noreply.github.com>
2021-05-04 11:29:36 +0900

    add trigger racer registration job

---
+++ app/jobs/trigger_racer_registration_job.rb
@@ -0,0 +1,11 @@
+class TriggerRacerRegistrationJob < ApplicationJob
+  def perform
+    race_entered_racer_registration_numbers = RaceEntry.pluck(:racer_registration_number).uniq
+    persisted_racer_registration_numbers = Racer.pluck(:registration_number).uniq
+    unexistence_racer_registration_numbers = race_entered_racer_registration_numbers - persisted_racer_registration_numbers
+
+    unexistence_racer_registration_numbers.first(10).each do |racer_registration_number|
+      RacerRegistrationJob.perform_later(racer_registration_number)
+    end
+  end
+end
