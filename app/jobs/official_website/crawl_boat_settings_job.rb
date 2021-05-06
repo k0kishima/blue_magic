17c0c271c9ad3f533b68415f87d6bdb37af83eb6
Johndoe <56298669+togane@users.noreply.github.com>
2021-05-07 08:15:37 +0900

    consider idempotency when crawl boat settings job

--- app/jobs/official_website/crawl_boat_settings_job.rb
+++ app/jobs/official_website/crawl_boat_settings_job.rb
@@ -1,5 +1,17 @@
 module OfficialWebsite
   class CrawlBoatSettingsJob < CrawlJob
+    # HACK: 本来このエラーは捨てていいのだが、このジョブだとCSV生成時の配列の構造が不正でも上がってくる
+    # そのケースでリトライしたら成功することがあるので一応再実行制御を入れている
+    # TODO: 正式に調査する
+    retry_on ArgumentError, wait: 1.minutes, attempts: 2 do |job, errror|
+      message = {
+        error_message: error.message,
+        job_name: job.class.name,
+        arguments: job.arguments.to_s,
+      }.map { |key, value| [key, value].join(': ') }.join("\n")
+      job.notify_error(message)
+    end
+
     def perform(stadium_tel_code:, race_opened_on:, race_number:, version: DEFAULT_VERSION)
       args = {
         version: version, stadium_tel_code: stadium_tel_code,
