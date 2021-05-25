d36666612b1aa45ea4403178fffe0c0b722d829f
Johndoe <56298669+togane@users.noreply.github.com>
2021-05-26 05:37:47 +0900

    fix formation model

--- app/models/formation.rb
+++ app/models/formation.rb
@@ -1,5 +1,16 @@
 class Formation
   def initialize(candicate_pit_numbers_list)
+    # HACK: 型宣言とかでうまく書けないものか(この場合数値の配列3つ持った2次元配列であることを想定している)
+    # もっと言えば末端の要素である数値は1..6の範疇である必要もある
+    raise ArgumentError, "the argument must be an array" unless candicate_pit_numbers_list.is_a?(Array)
+
+    array_size = candicate_pit_numbers_list.size
+    raise ArgumentError, "formation can receive three element array only (#{array_size})" unless array_size == 3
+
+    unless candicate_pit_numbers_list.all? { |array| array.all? { |number| number.is_a?(Integer) } }
+      raise ArgumentError, "the argument must be array of array which has only integer array"
+    end
+
     @candicate_pit_numbers_list = candicate_pit_numbers_list
   end
 
--- spec/models/formation_spec.rb
+++ spec/models/formation_spec.rb
@@ -6,10 +6,50 @@ describe Formation, type: :model do
   describe '#betting_numbers' do
     subject { formation.betting_numbers }
 
-    let(:candicate_pit_numbers_list) { [[1, 2], [1, 2], [1, 2, 3, 4, 5, 6]] }
+    context 'when given an array' do
+      context 'when the array size is 3' do
+        context 'when the array include numbers only' do
+          let(:candicate_pit_numbers_list) { [[1, 2], [1, 2], [1, 2, 3, 4, 5, 6]] }
 
-    it 'returns betting_numbers' do
-      expect(subject).to eq [123, 124, 125, 126, 213, 214, 215, 216]
+          it 'returns betting_numbers' do
+            expect(subject).to eq [123, 124, 125, 126, 213, 214, 215, 216]
+          end
+        end
+
+        context 'when the array include an empty array' do
+          let(:candicate_pit_numbers_list) { [[], [1, 2], [1, 2, 3, 4, 5, 6]] }
+
+          it 'returns blank array' do
+            expect(subject).to eq []
+          end
+        end
+
+        context 'when the array include empty arrays only' do
+          let(:candicate_pit_numbers_list) { [[], [], []] }
+
+          it 'returns blank array' do
+            expect(subject).to eq []
+          end
+        end
+
+        context 'when the array includes something not a number' do
+          let(:candicate_pit_numbers_list) { [[1, 2], [1, 2], [1, 2, 3, 4, 5, "6"]] }
+
+          it { expect { subject }.to raise_error(ArgumentError) }
+        end
+      end
+
+      context 'when the array size is not 3' do
+        let(:candicate_pit_numbers_list) { [[1, 2], [1, 2]] }
+
+        it { expect { subject }.to raise_error(ArgumentError) }
+      end
+    end
+
+    context 'when not given an array' do
+      let(:candicate_pit_numbers_list) { "12-12-123456" }
+
+      it { expect { subject }.to raise_error(ArgumentError) }
     end
   end
 end
