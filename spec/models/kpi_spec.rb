af57fa5d0dcc2a24cbe9296022cd99ef45f21b0b
Johndoe <56298669+togane@users.noreply.github.com>
2021-05-29 13:28:26 +0900

    fix kpi spec

--- spec/models/kpi_spec.rb
+++ spec/models/kpi_spec.rb
@@ -1,60 +1,11 @@
 require 'rails_helper'
 
-class DummyCalculator
-  def calculate!(entry_object)
-    true
-  end
-end
-
 RSpec.describe Kpi, type: :model do
   describe 'validation' do
     it { is_expected.to validate_presence_of(:entry_object_class_name) }
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:attribute_name) }
     it { is_expected.to allow_value(nil).for(:entry_object) }
-  end
-
-  describe '#value!' do
-    subject { kpi.value! }
-
-    describe 'validation' do
-      let(:kpi) {
-        Kpi.find_by(entry_object_class_name: 'RaceEntry',
-                    attribute_name: 'nige_succeed_rate_on_start_course_in_exhibition')
-      }
-
-      context 'when entry object is present' do
-        before do
-          kpi.entry_object = entry_object
-        end
-
-        context 'when entry object is an instance of specified class' do
-          let(:entry_object) { build(:race_entry) }
-
-          context 'when calculator is present' do
-            before do
-              kpi.calculator = DummyCalculator.new
-            end
-
-            it { is_expected.to be true }
-          end
-
-          context 'when calculator is blank' do
-            it { expect { subject }.to raise_error(ActiveRecord::RecordInvalid) }
-          end
-        end
-
-        context 'when entry object is not an instance of specified class' do
-          let(:entry_object) { build(:race) }
-
-          it { expect { subject }.to raise_error(ActiveRecord::RecordInvalid) }
-        end
-      end
-
-      context 'when entry object is blank' do
-        it { expect { subject }.to raise_error(ActiveRecord::RecordInvalid) }
-      end
-    end
   end
 end
 
