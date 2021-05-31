d0b6671907a2de48a8c163c5c68c031d057a44a1
Johndoe <56298669+togane@users.noreply.github.com>
2021-06-01 07:13:08 +0900

    fix annotations

--- app/models/racer_winning_trick_kpi.rb
+++ app/models/racer_winning_trick_kpi.rb
@@ -48,3 +48,21 @@ class RacerWinningTrickKpi < Kpi
     raise DataNotPrepared, 'the source object does not have exhibition data yet'
   end
 end
+
+# == Schema Information
+#
+# Table name: kpis
+#
+#  id                                                                                                                                                                                      :bigint           not null, primary key
+#  type                                                                                                                                                                                    :string(255)      not null
+#  entry_object_class_name(値の算出をするために利用するオブジェクトのことをentry object と定義し、そのクラス名をここで指定（webpackのエントリーポイントとのアナロジーからこのように命名）) :string(255)      not null
+#  name                                                                                                                                                                                    :string(255)      not null
+#  description                                                                                                                                                                             :text(65535)
+#  attribute_name                                                                                                                                                                          :string(255)      not null
+#  created_at                                                                                                                                                                              :datetime         not null
+#  updated_at                                                                                                                                                                              :datetime         not null
+#
+# Indexes
+#
+#  index_kpis_on_entry_object_class_name_and_attribute_name  (entry_object_class_name,attribute_name) UNIQUE
+#
