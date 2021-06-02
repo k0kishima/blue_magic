54fb952d4cddcafcc28e12a5519fa3ff469d8d86
Johndoe <56298669+togane@users.noreply.github.com>
2021-06-03 07:13:40 +0900

    implement forecast method in forecaster model

--- app/models/forecaster.rb
+++ app/models/forecaster.rb
@@ -1,6 +1,9 @@
 class Forecaster < ApplicationRecord
+  class AlreadyForecasted < StandardError; end
+
   has_many :forecasters_forecasting_patterns
   has_many :forecasting_patterns, through: :forecasters_forecasting_patterns
+  has_many :recommend_odds, through: :forecasters_forecasting_patterns, class_name: 'RecommendOdds'
 
   enum status: { active: 1, simulating: 2 }
   enum betting_strategy: { take_the_first_forecasting_pattern: 1, take_all_forecasting_patterns: 2 }
@@ -11,6 +14,16 @@ class Forecaster < ApplicationRecord
 
   def self.current
     raise NotImplementedError
+  end
+
+  def forecast!(race)
+    forecasters_forecasting_patterns.each do |forecasters_forecasting_pattern|
+      forecasters_forecasting_pattern.create_recommend_odds_of!(race)
+    end
+
+    recommend_odds.where(**race.attributes.slice(*Race.primary_keys))
+  rescue ActiveRecord::RecordNotUnique
+    raise AlreadyForecasted
   end
 end
 
