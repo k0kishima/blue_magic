0e6fc91403bfe9381c9c93720132c9e4699bb173
Johndoe <56298669+togane@users.noreply.github.com>
2021-06-05 05:43:55 +0900

    tweak forecasting models

--- db/migrate/20210531215606_fix_forecaster_table.rb
+++ db/migrate/20210531215606_fix_forecaster_table.rb
@@ -1,6 +1,6 @@
 class FixForecasterTable < ActiveRecord::Migration[6.1]
   def change
     remove_column :forecasters, :reduce_odds_method, :integer
-    add_column :forecasters, :betting_strategy, :integer, after: :description
+    add_column :forecasters, :betting_strategy, :integer, null: false, after: :description
   end
 end
