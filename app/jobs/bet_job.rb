09ce4bf1d264da23fdb74acf955842c9fcbdbbc5
Johndoe <56298669+togane@users.noreply.github.com>
2021-06-05 13:12:33 +0900

    fix bet job

--- app/jobs/bet_job.rb
+++ app/jobs/bet_job.rb
@@ -1,10 +1,14 @@
 class BetJob < ApplicationJob
-  discard_on ActiveRecord::RecordNotFound, Forecaster::AlreadyForecasted
+  class AnyForecastingPatternsDoNotMatched < StandardError; end
+
+  discard_on ActiveRecord::RecordNotFound, Forecaster::AlreadyForecasted, AnyForecastingPatternsDoNotMatched
 
   def perform(forecaster_id:, stadium_tel_code:, race_opened_on:, race_number:)
     race = Race.find_by!(stadium_tel_code: stadium_tel_code, date: race_opened_on, race_number: race_number)
     forecaster = Forecaster.find(forecaster_id)
     recommend_odds = forecaster.forecast!(race)
+    raise AnyForecastingPatternsDoNotMatched.new if recommend_odds.blank?
+
     betting_strategy_class = "BettingStrategy::#{forecaster.betting_strategy.camelize}".constantize
     betting_strategy = betting_strategy_class.new(recommend_odds: recommend_odds)
     betting_strategy.bet!
