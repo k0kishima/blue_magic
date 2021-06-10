55e7cdae64f3a9b1375c817f3bdf9564a4434538
Johndoe <56298669+togane@users.noreply.github.com>
2021-06-11 05:46:26 +0900

    modify race entry spec

--- spec/models/race_entry_spec.rb
+++ spec/models/race_entry_spec.rb
@@ -21,6 +21,214 @@ describe RaceEntry, type: :model do
     it { is_expected.to validate_inclusion_of(:pit_number).in_range(1..6) }
     it { is_expected.to validate_presence_of(:racer_registration_number) }
   end
+
+  describe 'readers about motors' do
+    describe '#motor_number' do
+      subject { race_entry.motor_number }
+
+      context 'when boat setting is present' do
+        let(:boat_setting) {
+          create(:boat_setting, **race_entry.attributes.slice(*RaceEntry.primary_keys), motor_number: 1)
+        }
+
+        before do
+          boat_setting
+        end
+
+        it 'returns motor number' do
+          expect(subject).to eq 1
+        end
+      end
+
+      context 'when boat setting is blank' do
+        it { expect { subject }.to raise_error(Module::DelegationError) }
+      end
+    end
+
+    describe '#motor_quinella_rate' do
+      subject { race_entry.motor_quinella_rate }
+
+      context 'when boat setting is present' do
+        let(:boat_setting) {
+          create(:boat_setting, **race_entry.attributes.slice(*RaceEntry.primary_keys), motor_number: 1)
+        }
+
+        before do
+          boat_setting
+        end
+
+        context 'when motor_betting_contribute_rate_aggregation is present' do
+          let(:motor_betting_contribute_rate_aggregation) {
+            create(:motor_betting_contribute_rate_aggregation,
+                   aggregated_on: race_entry.date,
+                   stadium_tel_code: race_entry.stadium_tel_code,
+                   motor_number: 1,
+                   quinella_rate: 33.3)
+          }
+
+          before do
+            motor_betting_contribute_rate_aggregation
+          end
+
+          it 'returns motor number' do
+            expect(subject).to eq 33.3
+          end
+        end
+
+        context 'when motor_betting_contribute_rate_aggregation is blank' do
+          it { expect { subject }.to raise_error(NoMethodError) }
+        end
+      end
+
+      context 'when boat setting is blank' do
+        it { expect { subject }.to raise_error(Module::DelegationError) }
+      end
+    end
+
+    describe '#motor_trio_rate' do
+      subject { race_entry.motor_trio_rate }
+
+      context 'when boat setting is present' do
+        let(:boat_setting) {
+          create(:boat_setting, **race_entry.attributes.slice(*RaceEntry.primary_keys), motor_number: 1)
+        }
+
+        before do
+          boat_setting
+        end
+
+        context 'when motor_betting_contribute_rate_aggregation is present' do
+          let(:motor_betting_contribute_rate_aggregation) {
+            create(:motor_betting_contribute_rate_aggregation,
+                   aggregated_on: race_entry.date,
+                   stadium_tel_code: race_entry.stadium_tel_code,
+                   motor_number: 1,
+                   trio_rate: 33.3)
+          }
+
+          before do
+            motor_betting_contribute_rate_aggregation
+          end
+
+          it 'returns motor number' do
+            expect(subject).to eq 33.3
+          end
+        end
+
+        context 'when motor_betting_contribute_rate_aggregation is blank' do
+          it { expect { subject }.to raise_error(NoMethodError) }
+        end
+      end
+
+      context 'when boat setting is blank' do
+        it { expect { subject }.to raise_error(Module::DelegationError) }
+      end
+    end
+  end
+
+  describe 'readers about boats' do
+    describe '#boat_number' do
+      subject { race_entry.boat_number }
+
+      context 'when boat setting is present' do
+        let(:boat_setting) {
+          create(:boat_setting, **race_entry.attributes.slice(*RaceEntry.primary_keys), boat_number: 1)
+        }
+
+        before do
+          boat_setting
+        end
+
+        it 'returns boat number' do
+          expect(subject).to eq 1
+        end
+      end
+
+      context 'when boat setting is blank' do
+        it { expect { subject }.to raise_error(Module::DelegationError) }
+      end
+    end
+
+    describe '#boat_quinella_rate' do
+      subject { race_entry.boat_quinella_rate }
+
+      context 'when boat setting is present' do
+        let(:boat_setting) {
+          create(:boat_setting, **race_entry.attributes.slice(*RaceEntry.primary_keys), boat_number: 1)
+        }
+
+        before do
+          boat_setting
+        end
+
+        context 'when boat_betting_contribute_rate_aggregation is present' do
+          let(:boat_betting_contribute_rate_aggregation) {
+            create(:boat_betting_contribute_rate_aggregation,
+                   aggregated_on: race_entry.date,
+                   stadium_tel_code: race_entry.stadium_tel_code,
+                   boat_number: 1,
+                   quinella_rate: 33.3)
+          }
+
+          before do
+            boat_betting_contribute_rate_aggregation
+          end
+
+          it 'returns boat number' do
+            expect(subject).to eq 33.3
+          end
+        end
+
+        context 'when boat_betting_contribute_rate_aggregation is blank' do
+          it { expect { subject }.to raise_error(NoMethodError) }
+        end
+      end
+
+      context 'when boat setting is blank' do
+        it { expect { subject }.to raise_error(Module::DelegationError) }
+      end
+    end
+
+    describe '#boat_trio_rate' do
+      subject { race_entry.boat_trio_rate }
+
+      context 'when boat setting is present' do
+        let(:boat_setting) {
+          create(:boat_setting, **race_entry.attributes.slice(*RaceEntry.primary_keys), boat_number: 1)
+        }
+
+        before do
+          boat_setting
+        end
+
+        context 'when boat_betting_contribute_rate_aggregation is present' do
+          let(:boat_betting_contribute_rate_aggregation) {
+            create(:boat_betting_contribute_rate_aggregation,
+                   aggregated_on: race_entry.date,
+                   stadium_tel_code: race_entry.stadium_tel_code,
+                   boat_number: 1,
+                   trio_rate: 33.3)
+          }
+
+          before do
+            boat_betting_contribute_rate_aggregation
+          end
+
+          it 'returns boat number' do
+            expect(subject).to eq 33.3
+          end
+        end
+
+        context 'when boat_betting_contribute_rate_aggregation is blank' do
+          it { expect { subject }.to raise_error(NoMethodError) }
+        end
+      end
+
+      context 'when boat setting is blank' do
+        it { expect { subject }.to raise_error(Module::DelegationError) }
+      end
+    end
+  end
 end
 
 # == Schema Information
