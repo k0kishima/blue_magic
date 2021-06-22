991cd3f0443e6b27a9aea67782b5253dc18cc35a
Johndoe <56298669+togane@users.noreply.github.com>
2021-06-22 20:05:58 +0900

    fix binary expressions

--- app/models/binary_expression_factory.rb
+++ app/models/binary_expression_factory.rb
@@ -12,6 +12,18 @@ class BinaryExpressionFactory
       end
     end
 
-    ->(object) { left_operand_processor.call(object).try(operator, right_operand_processor.call(object)) }
+    ->(object) do
+      # 今期F回数などのKPI値が入る
+      value_to_be_compared = left_operand_processor.call(object)
+
+      if value_to_be_compared.nil?
+        raise DataNotFound, "cannot operate #{hash} because of nil"
+      end
+
+      # それを閾値と比較する
+      # 以下だと閾値は1で、これをoperatorである >= で比較している
+      # {">="=>[{"item"=>"pit_number_1", "attribute"=>"flying_count_in_current_rating_term"}, {"item"=>"literal", "value"=>1}]}
+      value_to_be_compared.try(operator, right_operand_processor.call(object))
+    end
   end
 end
