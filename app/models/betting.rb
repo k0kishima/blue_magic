73231048c12249e8df22a5334950e91ab57c7234
Johndoe <56298669+togane@users.noreply.github.com>
2021-06-24 20:26:56 +0900

    fix betting model

--- app/models/betting.rb
+++ app/models/betting.rb
@@ -2,12 +2,15 @@ class Betting < ApplicationRecord
   include RaceAssociating
   include BettingMethodSelector
 
-  self.primary_keys = %i[forecasters_forecasting_pattern_id stadium_tel_code date race_number]
+  self.primary_keys = %i[forecasters_forecasting_pattern_id stadium_tel_code date race_number betting_number]
 
   belongs_to :forecasters_forecasting_pattern
+  belongs_to :recommend_odds, class_name: 'RecommendOdds', foreign_key: self.primary_keys
 
   validates :betting_amount, presence: true
   validates :voted_at, presence: true
+
+  delegate :ratio_when_forecasting, to: :recommend_odds
 
   def betting_numbers
     betting_number.to_s.split('').map(&:to_i)
@@ -23,7 +26,7 @@ end
 #  date                               :date             not null, primary key
 #  race_number                        :integer          not null, primary key
 #  betting_method                     :integer          not null
-#  betting_number                     :integer          not null
+#  betting_number                     :integer          not null, primary key
 #  betting_amount                     :integer          not null
 #  refunded_amount                    :integer
 #  adjustment_amount                  :integer
