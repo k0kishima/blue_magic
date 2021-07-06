4a8828975e73d4256b56e380d1e482924fae007e
Johndoe <56298669+togane@users.noreply.github.com>
2021-07-06 21:02:58 +0900

    cache calculate racer trick succeed rate results

--- app/models/racer_winning_trick_succeed_rate_calculator.rb
+++ app/models/racer_winning_trick_succeed_rate_calculator.rb
@@ -20,22 +20,24 @@ class RacerWinningTrickSucceedRateCalculator
   def calculate!(aggregation_range:)
     validate!
 
-    race_records =
-      RaceRecord
-      .joins(:race_entry)
-      .where(course_number: course_number)
-      .where(date: aggregation_range)
-      .merge(RaceEntry.where(racer_registration_number: racer_registration_number))
+    Rails.cache.fetch(cache_key(aggregation_range), expires_in: 1.hour) do
+      race_records =
+        RaceRecord
+        .joins(:race_entry)
+        .where(course_number: course_number)
+        .where(date: aggregation_range)
+        .merge(RaceEntry.where(racer_registration_number: racer_registration_number))
 
-    won_by_specified_trick_races =
-      race_records
-      .joins(:winning_race_entry)
-      .merge(WinningRaceEntry.where(winning_trick: trick.id))
+      won_by_specified_trick_races =
+        race_records
+        .joins(:winning_race_entry)
+        .merge(WinningRaceEntry.where(winning_trick: trick.id))
 
-    begin
-      Rational(won_by_specified_trick_races.count, race_records.count).to_f
-    rescue ZeroDivisionError
-      0
+      begin
+        Rational(won_by_specified_trick_races.count, race_records.count).to_f
+      rescue ZeroDivisionError
+        0
+      end
     end
   end
 
@@ -46,5 +48,20 @@ class RacerWinningTrickSucceedRateCalculator
     return if course_number.in?(trick.available_course_numbers)
 
     errors.add(:course_number, "#{course_number} cannot calculate at the #{trick.class.name}")
+  end
+
+  def cache_key(aggregation_range)
+    [
+      self.class.name.underscore,
+      Digest::MD5.hexdigest(
+        [
+          racer_registration_number,
+          trick.id,
+          course_number,
+          aggregation_range.first,
+          aggregation_range.last,
+        ].map(&:to_s).join('-')
+      )
+    ].join(':')
   end
 end
