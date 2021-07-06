b804674c0b5065132c4a2b030ecd5dc3b1700ae4
Johndoe <56298669+togane@users.noreply.github.com>
2021-07-06 19:59:58 +0900

    fix binary expressions

--- app/models/binary_expression_factory.rb
+++ app/models/binary_expression_factory.rb
@@ -17,10 +17,18 @@ class BinaryExpressionFactory
       value_to_be_compared = left_operand_processor.call(object)
 
       if value_to_be_compared.nil?
-        raise DataNotFound, "cannot operate #{hash} because of nil"
+        # 本来なら以下のような対応にしたい
+        # raise DataNotFound, "cannot operate #{hash} because of nil"
+        #
+        # 今の実装は and や or でグルーピングした全式を評価している
+        # つまり or は最初の条件が false であれば false をリターンするという短絡実行ではなくて、続く条件も評価されてしまう
+        # それらで例外があがったらそこで処理が止まってしまう
+        # 理想的には or が言語の論理演算子と同じように振る舞い短絡実行をしてくれることが望ましいが、
+        # 今は暫定的に例外が起こりうるケースでも false を返して処理が止まらないように暫定対応している
+        return false
       end
 
-      # それを閾値と比較する
+      # KPI値を閾値と比較する
       # 以下だと閾値は1で、これをoperatorである >= で比較している
       # {">="=>[{"item"=>"pit_number_1", "attribute"=>"flying_count_in_current_rating_term"}, {"item"=>"literal", "value"=>1}]}
       value_to_be_compared.try(operator, right_operand_processor.call(object))
