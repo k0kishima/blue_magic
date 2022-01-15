e22ebdc34ce6215bcb01a2a4e1cff21264cbc103
Johndoe <56298669+togane@users.noreply.github.com>
2022-01-15 11:00:43 +0900

    add a comment

--- app/models/concerns/race_associating.rb
+++ app/models/concerns/race_associating.rb
@@ -1,3 +1,6 @@
+# TODO: 名前変更
+# HasRaceAssociating みたいな方がいいと思う
+# そもそも Associating って書いてあるのに belongs_to などの関連がないので名前と実態に乖離がある
 module RaceAssociating
   extend ActiveSupport::Concern
   include StadiumAssociating
