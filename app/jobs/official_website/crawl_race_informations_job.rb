88367dd9435e692eace2509b80ff68554b880ae5
Johndoe <56298669+togane@users.noreply.github.com>
2022-10-06 21:41:46 +0900

    fix caching about race information crawling

--- app/jobs/official_website/crawl_race_informations_job.rb
+++ app/jobs/official_website/crawl_race_informations_job.rb
@@ -7,7 +7,12 @@ module OfficialWebsite
         version: version, stadium_tel_code: stadium_tel_code,
         race_opened_on: race_opened_on, race_number: race_number
       )
-      page.reload! if need_to_realod?
+
+      # hack:
+      # CrawlOpenedOrWillOpenRacesJob で朝にレースを収集した時のキャッシュが残っている
+      # そのキャッシュを利用してスクレイピングすると本番直前に更新された情報が取れない（安定板の着用や周回短縮など）
+      # 本来であれば引数などでリロードフラグ渡すなどしてここで判定のロジックを持たないのが好ましいが、一時的にこれで対応
+      page.reload! if need_to_realod? || race_opened_on == Date.today
       crawler = Crawler.new(page)
       crawler.crawl!
     rescue ::DataNotFound
